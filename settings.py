3e9c62a72e75d9fe0caeb25ec19e071499a9ec8f
David Keck <dstevenkeck@gmail.com>
2014-04-29 17:09:47 -0500

    slight style changes

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ __author__ = 'David Keck'
 CHOICES = 5
 
 ENCOURAGEMENT = ["That is correct! Good job! :)\n", "Yes! Way to go!\n", "You rock! That's right.\n",
-                 "That's right. I'm impressed!\n", "Whoa! That's correct!\n", "You da (wo)man!\n"]
+                 "That's right. I'm impressed!\n", "Whoa! That's correct!\n", "That's right. You da (wo)man!\n"]
